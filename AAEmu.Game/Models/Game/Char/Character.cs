3be18215ef10a28d7a51f86778231eeb90a5a57f
yanlongli <ahlyl94@gmail.com>
2024-08-13 22:51:07 +0800

    [Game] Before teleportation, forcibly remove the character from any mount or vehicle they may be riding on,

--- AAEmu.Game/Models/Game/Char/Character.cs
+++ AAEmu.Game/Models/Game/Char/Character.cs
@@ -1641,6 +1641,8 @@ public partial class Character : Unit, ICharacter
                     SendErrorMessage(ErrorMessageType.ClosedZone,0,false);
                     await Task.Delay(2 * 1000,_unreleasedZoneTransportedOut.Token);
                 }
+                ForceDismount();
+                await Task.Delay(200);
                 var portal = PortalManager.Instance.GetClosestReturnPortal(Connection.ActiveChar);
                 // force transported out
                 Connection.ActiveChar.BroadcastPacket(
